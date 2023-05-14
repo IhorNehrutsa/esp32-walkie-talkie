36c9d879ffd562d1fe8b6e78df6e381dabcfd42b
IhorNehrutsa <Ihor.Nehrutsa@gmail.com>
2023-05-14 22:40:36 +0300

    Fix

--- lib/audio_output/src/DACOutput.cpp
+++ lib/audio_output/src/DACOutput.cpp
@@ -2,23 +2,19 @@
 
 #if CONFIG_IDF_TARGET_ESP32
 
-void DACOutput::start(uint32_t sample_rate)
+void DACOutput::start(unsigned int sample_rate)
 {
     // i2s config for writing both channels of I2S
     i2s_config_t i2s_config = {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX | I2S_MODE_DAC_BUILT_IN),
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 1)
-        .sample_rate = sample_rate, 
+        .sample_rate = sample_rate,
 #else
-        .sample_rate = (int)sample_rate, 
+        .sample_rate = (int)sample_rate,
 #endif
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
-#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 2, 0)
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_STAND_MSB),
-#else
-        .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S_MSB),        
-#endif
+        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
         .dma_buf_count = 2,
         .dma_buf_len = 1024,
--- lib/audio_output/src/I2SOutput.cpp
+++ lib/audio_output/src/I2SOutput.cpp
@@ -5,15 +5,15 @@ I2SOutput::I2SOutput(i2s_port_t i2s_port, i2s_pin_config_t &i2s_pins) : Output(i
 {
 }
 
-void I2SOutput::start(uint32_t sample_rate)
+void I2SOutput::start(unsigned int sample_rate)
 {
     // i2s config for writing both channels of I2S
     i2s_config_t i2s_config = {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 1)
-        .sample_rate = sample_rate, 
+        .sample_rate = sample_rate,
 #else
-        .sample_rate = (int)sample_rate, 
+        .sample_rate = (int)sample_rate,
 #endif
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
