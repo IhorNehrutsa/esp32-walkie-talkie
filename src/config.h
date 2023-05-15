4431d4756e51680cdef9c32598f7b3c11925fd30
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2023-05-15 09:07:49 +0300

    wip

--- src/config.h
+++ src/config.h
@@ -18,20 +18,20 @@
 // Generally they will default to LEFT - but you may need to attach the L/R pin to GND
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
 // #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_RIGHT
-#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_18
-#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_19
-#define I2S_MIC_SERIAL_DATA GPIO_NUM_21
+#define I2S_MIC_SERIAL_CLOCK GPIO_NUM_7
+#define I2S_MIC_LEFT_RIGHT_CLOCK GPIO_NUM_6
+#define I2S_MIC_SERIAL_DATA GPIO_NUM_4
 
 // Analog Microphone Settings - ADC1_CHANNEL_7 is GPIO35
 #define ADC_MIC_CHANNEL ADC1_CHANNEL_7
 
 // speaker settings
 #define USE_I2S_SPEAKER_OUTPUT
-#define I2S_SPEAKER_SERIAL_CLOCK GPIO_NUM_18
-#define I2S_SPEAKER_LEFT_RIGHT_CLOCK GPIO_NUM_19
+#define I2S_SPEAKER_SERIAL_CLOCK GPIO_NUM_7
+#define I2S_SPEAKER_LEFT_RIGHT_CLOCK GPIO_NUM_6
 #define I2S_SPEAKER_SERIAL_DATA GPIO_NUM_5
 // Shutdown line if you have this wired up or -1 if you don't
-#define I2S_SPEAKER_SD_PIN GPIO_NUM_22
+#define I2S_SPEAKER_SD_PIN -1 // GPIO_NUM_22
 
 // transmit button
 #define GPIO_TRANSMIT_BUTTON 23
@@ -47,7 +47,7 @@
 // On which wifi channel (1-11) should ESP-Now transmit? The default ESP-Now channel on ESP32 is channel 1
 #define ESP_NOW_WIFI_CHANNEL 1
 
-// In case all transport packets need a header (to avoid interference with other applications or walkie talkie sets), 
+// In case all transport packets need a header (to avoid interference with other applications or walkie talkie sets),
 // specify TRANSPORT_HEADER_SIZE (the length in bytes of the header) in the next line, and define the transport header in config.cpp
 #define TRANSPORT_HEADER_SIZE 0
 extern uint8_t transport_header[TRANSPORT_HEADER_SIZE];
