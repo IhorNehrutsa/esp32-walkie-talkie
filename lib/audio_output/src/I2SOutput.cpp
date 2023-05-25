32703454daf3ea5f870dc0cf5ab2b0b89107344b
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2023-05-25 10:50:07 +0300

    Ok +Walk - SELF

--- lib/audio_output/src/I2SOutput.cpp
+++ lib/audio_output/src/I2SOutput.cpp
@@ -9,13 +9,13 @@ void I2SOutput::start(uint32_t sample_rate)
 {
     // i2s config for writing both channels of I2S
     i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX),
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 1)
-        .sample_rate = sample_rate, 
+        .sample_rate = sample_rate,
 #else
-        .sample_rate = (int)sample_rate, 
+        .sample_rate = (int)sample_rate,
 #endif
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT, // I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 2, 0)
         .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_STAND_I2S),
